69b2a875407270e580811dec77344e43b904c2be
Dogurasu53 <douglasduartecoeho@gmail.com>
2019-12-02 18:50:19 -0300

    Massive changes to color layout of character page

--- application/views/persona/wiki.php
+++ application/views/persona/wiki.php
@@ -1,11 +1,11 @@
 <div class="container" style="margin-top: 133px;">
     <div class="row">
-        <div class="col-8" style="background: gray;">
+        <div class="col-8" style="background: #003838;">
             <div class="description" style="margin: 15px;">
                 The path of the martial artist is like a flame. Practicing the martial arts will only cause pain. The dangers are countless and the road is filled with obstacles. Everyone who walks down it will eventually turn to ash, but the true martial artist will be reborn from these ashes. Even if I was only a small and weak moth, I will walk into the flames without hesitation. I will fight my destiny for a one in a million chance that I will experience my own samsara and be reborn into a flaming phoenix. And even now, I am no longer a moth.
                 </br></br>
                 Lin Ming is the main character of the novel Martial World. Once a common youth with average talent, until he found a strange cube that changed his destiny and set him on the path to becoming the greatest cultivator in the 33 heavens and 18 abysses.
-                <div class="header" style="background: tomato;margin: 20px 0;">
+                <div class="header" style="background: #002A32;margin: 20px 0;text-align: center;">
                     Powers and Stats
                 </div>
                 <div>
@@ -16,8 +16,8 @@
                 </div>
             </div>
         </div>
-        <div class="col-4" style="background: blue;">
-            <div class="row" style="text-align: center;display: block;font-size: 18pt;margin-bottom: 5px;background: teal;font-weight: bold;line-height: 50px;">
+        <div class="col-4" style="background: #544C4D; font-family: Palatino, Palatino Linotype, Palatino LT STD, Book Antiqua, Georgia, serif; font-size: 18px; font-weight: bold; color: white;">
+            <div class="row" style="text-align: center;display: block;font-size: 18pt;margin-bottom: 5px;background: #002A32;font-weight: bold;line-height: 50px;">
                 <?php echo $persona->name ?>
             </div>
             <div class="row" style="text-align: center;display: block;">
